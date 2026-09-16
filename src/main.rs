05f1d2db09ec749fdf151359ce50f648a3e30e7f
pajeanbe <jeanbertropaul@gmail.com>
2026-09-16 22:56:41 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use omnisor::{DeviceSession, CiscoVariant};
+use rustmiko::devices::cisco::CiscoSSH;
 
-#[tokio::main]
-async fn main() -> Result<(), omnisor::Error> {
-    // Connexion à un commutateur/routeur Cisco (IOS)
-    let mut session = DeviceSession::connect(
-        ("192.168.1.1", 22),
-        "controller",
-        "controller123@",
-        CiscoVariant::Ios, // Utilisez IosLegacy pour les anciens matériels
-    ).await?;
-
-    // Envoyer une commande et récupérer le résultat
-    let output = session.send_command("show ip interface brief").await?;
-    println!("{:?}", output);
+fn main() -> anyhow::Result<()> {
+    let mut cisco = match CiscoSSH::connect("192.168.1.1:23", "controller", "controller123@") {
+        Ok(cisco) => {
+            println!("Connected successfully");
+            cisco
+        },
+        Err(e) => {
+            eprintln!("Failed to connect: {}", e);
+            return Ok(());
+        },
+    };
 
     Ok(())
 }
