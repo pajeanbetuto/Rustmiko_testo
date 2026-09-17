f17fb1981fe18de37df09b28ac39286e0dad09e6
pajeanbe <jeanbertropaul@gmail.com>
2026-09-17 23:14:47 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 use ssh2::Session;
-use std::io::prelude::*;
+use std::io::Read;
 use std::net::TcpStream;
 
-fn main() {
-    let tcp = TcpStream::connect("192.168.1.1:22").unwrap();
-    let mut sess = Session::new().unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 1. Connect to the Cisco node's TCP port 22 (SSH)
+    let tcp = TcpStream::connect("192.168.1.1:22")?;
+    let mut sess = Session::new()?;
     sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    sess.userauth_password("controller", "controller123@")
-        .unwrap();
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("sh mac address-table ").unwrap();
+    sess.handshake()?;
 
+    // 2. Authenticate with username and password
+    sess.userauth_password("admin", "your_password")?;
+
+    // 3. Open an SSH channel
+    let mut channel = sess.channel_session()?;
+
+    // 4. Send the command to the Cisco node
+    channel.exec("show version")?;
+
+    // 5. Read the output from the node
     let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("{}", s);
-    channel.wait_close().ok();
-    println!("{}", channel.exit_status().unwrap());
+    channel.read_to_string(&mut s)?;
+    print!("{}", s);
+
+    // 6. Close the channel cleanly
+    channel.wait_close()?;
+
+    Ok(())
 }
